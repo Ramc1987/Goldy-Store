4081cfd8aaad3dfa324c1f3f256ac756d451ddaa
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2023-01-30 15:42:37 +0300

    Adds section sale and popular-category

--- source/js/main.js
+++ source/js/main.js
@@ -2,15 +2,18 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {choiceJs} from './modules/project/choices';
 import {activateMobMenu} from './modules/project/mobile-menu';
+import {initSwiperPromo} from './modules/project/swiper-promo';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
+  const swiper = document.querySelector('.swiper');
 
   // Utils
   // ---------------------------------
 
   iosVhFix();
+  initSwiperPromo(swiper);
 
   // Modules
   // ---------------------------------
